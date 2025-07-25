9e6997738d6d622d141aad777616165653538ffa
Suman <suman.sumanshekhar698@gmail.com>
2025-07-26 00:42:44 +0530

    Added DSA Question - DP Question

---
+++ src/main/java/com/lpu/java/dsa/dp/_322CoinChange.java
@@ -0,0 +1,33 @@
+package com.lpu.java.dsa.dp;
+
+import java.util.Arrays;
+
+public class _322CoinChange {
+    public static void main(String[] args) {
+        int[] coins = {1, 3, 4, 7};
+        int min = coinChange(coins, 7);
+//        int[] coins = {2};
+//        int min = coinChange(coins, 3);
+        System.out.println(min);
+
+    }
+
+    static public int coinChange(int[] coin, int amount) {
+        int[] dp = new int[amount + 1];//0->amount
+        Arrays.fill(dp, amount + 1);//max amount beyond the amount
+        dp[0] = 0;//BASE Case
+
+//       O Time : O(amount*len(coins))
+//       O Space : O(amount)
+        for (int i = 1; i <= amount; i++) { // i is amount
+            for (int coinValue : coin) {
+                if (i - coinValue >= 0) {
+                    dp[i] = Math.min(dp[i], 1 + dp[i - coinValue]);//means min(current coins, 1 coin+dp[amount - currentCoinValue])
+                }//if a coin array is sorted, we can use break here to further optimize
+            }
+        }
+
+        return dp[amount] == Integer.MAX_VALUE - 1 ? -1 : dp[amount];
+
+    }
+}
